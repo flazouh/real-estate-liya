dfd3efb595fbb98114ed1165d491ef4c7a45b87d
flazouh <alexandredepape@gmail.com>
2025-04-10 14:53:10 +0300

    Update Calendly event link in ApartmentForm to direct users to the apartments showing schedule.

--- src/components/ApartmentForm.tsx
+++ src/components/ApartmentForm.tsx
@@ -410,7 +410,7 @@ export function ApartmentForm() {
                       />
                     ) : (
                       <InlineWidget
-                        url="https://calendly.com/liyakharitonova/30min"
+                        url="https://calendly.com/liyakharitonova/apartments-showing"
                         styles={{
                           height: "650px",
                           width: "100%",
